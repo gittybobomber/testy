18339ca568fc7262b867ac6fac87c135efbd47b0
Bobgit <bobgit@b-weg.de>
2024-03-12 14:51:06 +0100

    np

--- members/views.py
+++ members/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
 from django.template import loader
+import numpy as np
 
+x = [1,2,3]
+testnumber = np.mean(x)
+print(testnumber)
 def members(request):
   template = loader.get_template('basy.html')
-  return HttpResponse(template.render())
+  context = {
+    'testnum': testnumber
+  }
+  return HttpResponse(template.render(context))
